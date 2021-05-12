9d4d5da0ac5e66b9e7e8f9ed3d27ac90032a11a1
Vinicius Rossi <viniciuslimarossi7@gmail.com>
2021-05-12 20:57:09 -0300

    Deploy

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// Variáveis
+
+var div
+const links = []
+
+fetch('https://viniciusrossii.github.io/Repo.json/repo.json')
+    .then(response => response.json())
+    .then(response => {
+        for (var c = 0; c < response.sites.length; c++) {
+            links.push(response.sites[c].link)
+            if (c % 2 == 0) {
+                div = document.createElement('div')
+                div.classList.add("content")
+                document.getElementById('container').appendChild(div)
+            }
+            const span = document.createElement('span')
+            span.classList.add('span')
+            span.innerHTML = response.sites[c].name
+            div.appendChild(span)
+            setLinks(c)
+        }
+    })
+
+function setLinks(param) {
+    document.getElementsByClassName('span')[param].addEventListener('click', () => {
+        window.location.href = links[param]
+    })
+}
+
+document.getElementById('footer').addEventListener('click', () => {
+    window.location.href = "https://github.com/ViniciusRossii"
+})
